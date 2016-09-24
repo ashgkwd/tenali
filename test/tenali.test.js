bfac7e97bc4d9b838d3537702655db436173ae69
Ashish Gaikwad <ash.gkwd@gmail.com>
2016-09-24 12:48:50 +0530

    chore(tests): adds tests for tenali.js

---
+++ test/tenali.test.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var assert = chai.assert;
+var templates = require('./templates.json');
+var tenali = require('../src/tenali');
+
+describe('tenali', function () {
+	describe('Tenali()', function () {
+		it('should be a function', function () {
+			tenali.should.be.a('function');
+		});
+
+		it('should return an object', function () {
+			(new tenali()).should.have.property('register');
+		});
+
+		it('should register templates', function () {
+			expect(new tenali().register(templates)).to.be.undefined;
+		});
+
+		it('should return a store object', function () {
+			var tenaliInstance = new tenali()
+			tenaliInstance.register(templates);
+			expect(tenaliInstance.list()).to.satisfy(storeHash);
+
+			function storeHash(store) {
+				return store[templates[0].input][templates[0].variant][templates[0].engine] === templates[0].template;
+			}
+		});
+
+		it('should return a template string', function () {
+			var tenaliInstance = new tenali();
+			tenaliInstance.register(templates);
+			tenaliInstance.get(templates)[0].should.equal(templates[0].template);
+		});
+	})
+})
